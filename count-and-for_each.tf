0abd4f9c31082831168a03642513bde6dd1c6f92
amitkumardube <37042351+amitkumardube@users.noreply.github.com>
2021-02-28 22:22:27 +0200

    Update count-and-for_each.tf

--- count-and-for_each.tf
+++ count-and-for_each.tf
@@ -1,1 +1,15 @@
-Hello
+resource "type" "name" {
+  count = 2
+  tag = {
+    name = "customer-${count.index}"
+    }
+  }
+
+
+resource "type" "name" {
+  for_each = {
+      food = "public"
+      cash = "private"
+    }
+    bucket = "${each.key}-${var.suffix}"
+  }
