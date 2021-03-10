2b2bf62594660b9d387e381e921dcce0335ca5e8
amitkumardube <amitkumardube@gmail.com>
2021-03-10 10:58:15 +0200

    fixed the firewall block

--- for_each/firewall.tf
+++ for_each/firewall.tf
@@ -3,13 +3,11 @@ resource "google_compute_firewall" "for_each_firewall" {
   network = google_compute_network.for_each_network.name
 
   // this will ensure that it looks through the entire set of ports to create a dynamic content
-  dynamic "port" {
+  dynamic "allow" {
     for_each = var.ports
     content {
-      allow {
-        protocol = "tcp"
-        ports = [port.value]
-      }
+      protocol = "tcp"
+      ports = [allow.value]
     }
   }
 }
